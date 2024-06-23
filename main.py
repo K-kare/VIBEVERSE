a7810883f6a34edac04845122ba2eb8197cbb441
K-kare <kamunawangari@gmail.com>
2024-06-23 21:35:33 +0100

    new start

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-from App import create_app
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__)
 
-if __name__ == '__main__':
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", user_name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+
+if __name__== "__main__":
     app.run(debug=True)
