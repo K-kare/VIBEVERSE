c773d3906edb0d6e8dbbc11be1743e3bb3844878
K-kare <name_virgina@DESKTOP-FUGQNF4.localdomain>
2024-07-10 09:27:41 +0100

    First-project

--- main.py
+++ main.py
@@ -1,6 +1,335 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError, TextAreaField
+from wtforms.validators import DataRequired, EqualTo, Length
+from datetime import datetime
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import date
+from wtforms.widgets import TextArea
+from flask_ckeditor import CKEditor
+from flask_ckeditor import CKEditorField
+from flask_wtf.file import FileField
+from werkzeug.utils import secure_filename
+import uuid as uuid
+import os
 
 app = Flask(__name__)
+CKeditor = CKEditor(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'   
+app.config['SECRET_KEY'] = 'Awersome'
+UPLOAD_FOLDER = 'static/images/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+db = SQLAlchemy(app)
+migrate = Migrate(app, db, render_as_batch=True)
+app.app_context().push()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return users.query.get(int(user_id))
+
+@app.context_processor
+def base():
+    form = SearchForm()   
+    return dict(form=form)
+
+@app.route('/search', methods=["POST"])
+def search():
+    form = SearchForm()
+    posts = Posts.query
+    if form.validate_on_submit():
+        post.searched = form.searched.data
+        posts = posts.filter(Posts.content.like('%' + post.searched + '%'))
+        posts = posts.order_by(Posts.title).all()
+        return render_template("search.html", form=form, searched = post.searched)
+    
+
+
+class SearchForm(FlaskForm):
+    searched= StringField("searched", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+
+
+class LoginForm(FlaskForm):
+    username = StringField("username", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = users.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password_hash, form.password.data):
+                login_user(user)
+                flash("Login Succesfull")
+                return redirect(url_for('dashboard'))
+            else:
+                flash("Wrong Password - Try Again!")
+        else:
+            flash("That User Doesn't Exist! Try Again...")
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out")
+    return redirect(url_for('login'))
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    form = usersForm()
+    id = current_user.id
+    name_to_update = users.query.get_or_404(id)
+    if request.method == "POST":
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        name_to_update.favorite_color = request.form['favorite_color']
+        name_to_update.username = request.form['username']
+        name_to_update.about_author = request.form['about_author']
+        name_to_update.profile_pic = request.files['profile_pic']
+        pic_filename = secure_filename(name_to_update.profile_pic.filename)
+        pic_name = str(uuid.uuid1()) + "_" + pic_filename
+        name_to_update.profile_pic.save(os.path.join(app.config['UPLOAD_FOLDER']), pic_name)
+        name_to_update.profile_pic = pic_name
+        try:
+            db.session.commit()
+            flash("User Updated Successfully!")
+            return render_template("dashboard.html", form=form, name_to_update = name_to_update, id=id)
+        except:
+            flash("Error!  Looks like there was a problem...try again!")
+            return render_template("dashboard.html", form=form, name_to_update = name_to_update, id=id)
+    else:
+        return render_template("dashboard.html", form=form, name_to_update = name_to_update, id=id)
+    return render_template('dashboard.html')
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    #author = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    slug = db.Column(db.String(255))
+    poster_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    #content = StringField("Content", validators=[DataRequired()], widget=TextArea())
+    content = CKEditorField('Content',validators=[DataRequired()] )
+    author = StringField("Author")
+    slug = StringField("Slugfield", validators=[DataRequired()])
+    submit = SubmitField("Submit", validators=[DataRequired()])
+
+@app.route('/posts/delete/<int:id>')
+@login_required
+def delete_post(id):
+    post_to_delete = Posts.query.get_or_404(id)
+    id = current_user.id
+    if id == post_to_delete.poster.id:
+        try:
+            db.session.delete(post_to_delete)
+            db.session.commit()
+            flash("Blog post was deleted")
+            posts = Posts.query.order_by(Posts.date_posted)
+            return render_template("posts.html", posts=posts)
+        except:
+            flash("There was a prolem deleting post!!!")
+            posts = Posts.query.order_by(Posts.date_posted)
+            return render_template("posts.html", posts=posts)
+    else:
+        flash("Sorry you are not authorized to delete!!")
+        posts = Posts.query.order_by(Posts.date_posted)
+        return render_template("posts.html", posts=posts)
+
+@app.route('/posts')
+def posts():
+    posts = Posts.query.order_by(Posts.date_posted)
+    return render_template("posts.html", posts=posts)
+
+@app.route('/posts/<int:id>')
+def post(id):
+    post = Posts.query.get_or_404(id)
+    return render_template("post.html", post=post)
+
+@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+def edit_post(id):
+    post = Posts.query.get_or_404(id)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        #post.author = form.author.data
+        post.slug = form.slug.data
+        post.content = form.content.data
+        db.session.add(post)
+        db.session.commit()
+        flash("Post has been updated successfully")
+        return redirect(url_for('post', id=post.id))
+    if current_user.id == post.poster_id:
+        form.title.data = post.title
+        #form.author.data = post.author
+        form.slug.data = post.slug
+        form.content.data = post.content
+        return render_template('edit_post.html', form=form)
+    else:
+        flash("Sorry you are not authorised to edit this post")
+        posts = Posts.query.order_by(Posts.date_posted)
+        return render_template("posts.html", posts=posts)
+    
+
+
+@app.route('/add-post', methods=['GET', 'POST'])
+@login_required
+def add_post():
+    form = PostForm()
+
+    if form.validate_on_submit():
+        poster = current_user.id
+        post = Posts(title=form.title.data, content=form.content.data, poster_id=poster, slug=form.slug.data)
+        form.title.data=''
+        form.content.data=''
+        #form.author.data=''
+        form.slug.data=''
+
+        db.session.add(post)
+        db.session.commit()
+        flash("Blog Post Submitted  Successfully!")
+    return render_template("add_post.html", form=form)
+@app.route('/date')
+def get_current_date():
+    return{"Date": date.today()}
+
+
+
+
+class users(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    favorite_color = db.Column(db.String(120))
+    about_author = db.Column(db.Text(600), nullable=True,)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    profile_pic = db.Column(db.String(), nullable=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Posts', backref='poster')
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute!')
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def  verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+    
+    
+    def __repr__(self):
+        return '<Name %r>' % self.name
+    
+@app.route('/delete/<int:id>')
+def delete(id):
+    user_to_delete = users.query.get_or_404(id)
+    name = None
+    form = usersForm()
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash("User Deleted Successfully!!")
+        our_users = users.query.order_by(users.date_added)
+        return render_template("add_user.html",form=form,name=name,our_users=our_users)
+    except:
+         flash("Whoops! There was a problem deleting user, try again...")
+         return render_template("add_user.html", form=form, name=name,our_users=our_users)    
+
+
+
+class usersForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    favorite_color = StringField("Favorite Color")
+    about_author = TextAreaField("About Author")
+    password_hash =PasswordField('Password', validators=[DataRequired(), EqualTo('password_hash2', message='Passwords Must Match!')])
+    password_hash2=PasswordField('Confirm Password', validators=[DataRequired()])
+    profile_pic = FileField("Profile Pic")
+    submit= SubmitField("Submit")
+
+
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+@login_required
+def update(id):
+    form = usersForm()
+    name_to_update = users.query.get_or_404(id)
+    if request.method == "POST":
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        name_to_update.favorite_color = request.form['favorite_color']
+        name_to_update.username = request.form['username']
+        name_to_update.about_author = request.form['about_author']
+        try:
+            db.session.commit()
+            flash("User Updated Successfully!")
+            return render_template("update.html", form=form, name_to_update = name_to_update, id=id)
+        except:
+            flash("Error!  Looks like there was a problem...try again!")
+            return render_template("update.html", form=form, name_to_update = name_to_update, id=id)
+    else:
+        return render_template("update.html", form=form, name_to_update = name_to_update, id=id)
+    
+class PasswordForm(FlaskForm):
+    email = StringField("What's Your Email ", validators=[DataRequired()])
+    password_hash = PasswordField("What's Your Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+
+        
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = usersForm()
+    if form.validate_on_submit():
+        user = users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            hashed_pw = generate_password_hash(form.password_hash.data)
+            user = users(username=form.username.data,name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data, password_hash=hashed_pw)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.username.data=''
+        form.email.data = ''
+        form.favorite_color.data = ''
+        form.password_hash.data = ''
+        flash("User Added Successfully!")
+    our_users = users.query.order_by(users.date_added)
+    return render_template("add_user.html", form=form, name=name, our_users=our_users)    
+        
+
+
 
 @app.route('/')
 def index():
@@ -18,6 +347,35 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template("500.html"), 500
 
+@app.route('/test_pw', methods=['GET', 'POST'])
+def test_pw():
+    email = None
+    password = None
+    pw_to_check = None
+    passed = None
+    form = PasswordForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password_hash.data
+        form.email.data = ''
+        form.password_hash.data=''
+        flash("Form submitted successfully")
+        pw_to_check = users.query.filter_by(email=email).first()
+        passed = check_password_hash(pw_to_check.password_hash, password)
+    return render_template("test_pw.html", email = email, password = password, pw_to_check = pw_to_check, passed = passed, form = form)
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+	name = None
+	form = NamerForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+		flash("Form Submitted Successfully!")
+		
+	return render_template("names.html", 
+		name = name,
+		form = form)
 
 if __name__== "__main__":
     app.run(debug=True)
---
+++ migrations/versions/58374363cec3_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 58374363cec3
+Revises: 7a82431f90a0
+Create Date: 2024-07-04 11:42:13.724922
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '58374363cec3'
+down_revision = '7a82431f90a0'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
